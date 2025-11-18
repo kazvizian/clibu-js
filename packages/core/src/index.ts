29d8e8e78d7ed45466e160c76c767349ff4b1874
kazviz <137312421+kazviz@users.noreply.github.com>
2025-11-18 18:21:17 +0700

    feat: create public entrypoint for `@clibu/core` with module re-exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Public entrypoint for `@clibu/core`.
+ *
+ * Re-exports the primary modules that consumers (facade, help, loader, tests)
+ * should use when integrating with the core engine.
+ */
+export * from "./context"
+export * from "./errors"
+export * from "./graph"
+export * from "./organ"
+export * from "./parser"
+export * from "./run"
+export * from "./schema"
+export * from "./types"
